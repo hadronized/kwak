693189f6e3596e2234145128957e348da1dd013e
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2016-09-30 03:01:50 +0200

    Added latin-1 as accepted encoding.

--- src/main.rs
+++ src/main.rs
@@ -197,7 +197,8 @@ fn treat_privmsg(irc: &mut IRCClient, re_url: &Regex, re_title: &Regex, nick: Ni
         );
         headers.set(
           header::AcceptCharset(vec![
-            header::qitem(header::Charset::Ext("utf-8".to_owned()))
+            header::qitem(header::Charset::Ext("utf-8".to_owned())),
+            header::qitem(header::Charset::Iso_8859_1)
           ])
         );
         headers.set(header::UserAgent("hyper/0.9.10".to_owned()));
